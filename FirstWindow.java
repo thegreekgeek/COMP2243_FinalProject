fa2f2d582603071e066d1ccbd878b7a242e787f4
Robert Young <ki6254kq@nw.roch.edu>
2025-11-18 09:55:30 -0600

    did some stuff

--- FirstWindow.java
+++ FirstWindow.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 
+
 public class FirstWindow {
     public static void main(String[] args) {
         JFrame frame = new JFrame("First Window");
---
+++ GreetingMachine.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GreetingMachine extends JFrame {
+    public GreetingMachine() {
+        setTitle("Greeting Machine");
+        setSize(400, 150);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+
+        JLabel prompt = new JLabel("WHAT IS YOUR NAME");
+        JTextField nameField = new JTextField(15);
+        JLabel prompt2 = new JLabel("WHAT IS YOUR QUEST");
+        JTextField questField = new JTextField(15);
+        JButton greetButton = new JButton("GREET ME");
+
+        add(prompt);
+        add(nameField);
+        add(prompt2);
+        add(questField);
+        add(greetButton);
+
+        setVisible(true);
+
+        greetButton.addActionListener( e-> {
+            String name = nameField.getText();
+            String quest = questField.getText();
+            JOptionPane.showMessageDialog(this, "HELLO, " + name + "! \n" + "Your quest to" + quest + " has been derailed!");
+        });
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new GreetingMachine());
+    }
+}
